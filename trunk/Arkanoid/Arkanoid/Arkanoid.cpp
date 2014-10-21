7e9ef518145b633674a6b98c306e252377265d57
inprazia@gmail.com <inprazia@gmail.com@94d167a3-78b1-cb6c-489c-a40cd598ee0c>
2014-10-21 16:15:31 +0000

    Created a 3D ball with a texture into a slight fog. Moved the loading procedure after OpenGL initialization. Some cosmetic changes.

--- trunk/Arkanoid/Arkanoid/Arkanoid.cpp
+++ trunk/Arkanoid/Arkanoid/Arkanoid.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <deque>
 #include <vector>
+#include <math.h>
 #include <gl/gl.h>									// Header File For The OpenGL32 Library
 #include <gl/glu.h>									// Header File For The GLu32 Library
 
@@ -12,6 +13,10 @@
 
 #ifndef WM_TOGGLEFULLSCREEN							// Application Define Message For Toggling
 #	define WM_TOGGLEFULLSCREEN (WM_USER+1)									
+#endif
+
+#ifndef PI
+#	define PI 3.1415926535897932384626433832795f
 #endif
 
 #define MASK(start, end) ((~((~0L)<<((end)-(start)+1)))<<(start))
@@ -125,45 +130,45 @@ public:
 
 class Image
 {
-	std::vector<char> vBuffer;
+	std::vector<char> m_vBuffer;
+	WORD m_uWidth, m_uHeight;
+	BYTE m_uComps;
 public:
-	WORD uWidth, uHeight;
-	BYTE uComps;
+	Image():m_uWidth(0), m_uHeight(0), m_uComps(PIXEL_COMP_RGB){}
 
-	Image():uWidth(0),uHeight(0),uComps(PIXEL_COMP_RGB){}
-
-	char *GetDataPtr() { return vBuffer.size() > 0 ? &vBuffer[0] : NULL; }
-	size_t GetDataSize() const { return vBuffer.size(); }
-	WORD GetWidth() const { return uWidth; }
-	WORD GetHeight() const { return uHeight; }
-	BYTE GetComp() const { return uComps; }
+	char *GetDataPtr() { return m_vBuffer.size() > 0 ? &m_vBuffer[0] : NULL; }
+	const char *GetDataPtr() const { return m_vBuffer.size() > 0 ? &m_vBuffer[0] : NULL; }
+	size_t GetDataSize() const { return m_vBuffer.size(); }
+	WORD GetWidth() const { return m_uWidth; }
+	WORD GetHeight() const { return m_uHeight; }
+	BYTE GetComp() const { return m_uComps; }
 	
 	BOOL SetSize(int nNewWidth, int nNewHeight, int nNewComp = PIXEL_COMP_RGB)
 	{
 		if(nNewWidth <= 0 || nNewHeight <= 0 ||
 			nNewComp != PIXEL_COMP_GRAY && nNewComp != PIXEL_COMP_RGB && nNewComp != PIXEL_COMP_RGBA)
 			return FALSE;
-		vBuffer.resize(nNewWidth * nNewHeight * nNewComp);
-		uComps = nNewComp;
-		uWidth = nNewWidth;
-		uHeight = nNewHeight;
+		m_vBuffer.resize(nNewWidth * nNewHeight * nNewComp);
+		m_uComps = nNewComp;
+		m_uWidth = nNewWidth;
+		m_uHeight = nNewHeight;
 		return TRUE;
 	}
 	void Clear()
 	{
-		vBuffer.clear();
+		m_vBuffer.clear();
 	}
 	void FlipV()
 	{
 		char *pData = GetDataPtr();
-		const int my = uHeight/2, B = uComps * uWidth, D = (uHeight - 1) * B;
-		for(int i = 0 ; i < uWidth; i++)
+		const int my = m_uHeight/2, B = m_uComps * m_uWidth, D = (m_uHeight - 1) * B;
+		for(int i = 0 ; i < m_uWidth; i++)
 		{
-			const int A = uComps * i, C = A + D;
+			const int A = m_uComps * i, C = A + D;
 			for(int j = 0; j < my; j++)
 			{
 				const int p1 = A + j * B, p2 = C - j * B;
-				switch(uComps)
+				switch(m_uComps)
 				{
 				case PIXEL_COMP_RGBA:
 					REPLACE(pData[p1+3], pData[p2+3]);
@@ -174,6 +179,28 @@ public:
 					REPLACE(pData[p1], pData[p2]);
 				}
 			}
+		}
+	}
+	void Image::FlipC()
+	{
+		if(m_uComps<PIXEL_COMP_RGB)
+			return;
+		const int m=m_uComps;
+		const int s=m_uWidth*m_uHeight;
+		const void *d=GetDataPtr();
+		__asm							// Assembler Code To Follow
+		{
+			mov ecx, s					// Counter Set To Dimensions Of Our Memory Block
+			mov ebx, d					// Points ebx To Our Data (b)
+			label:						// Label Used For Looping
+			mov al,[ebx+0]				// Loads Value At ebx Into al
+			mov ah,[ebx+2]				// Loads Value At ebx+2 Into ah
+			mov [ebx+2],al				// Stores Value In al At ebx+2
+			mov [ebx+0],ah				// Stores Value In ah At ebx
+		
+			add ebx,m					// Moves Through The Data
+			dec ecx						// Decreases Our Loop Counter
+			jnz label					// If Not Zero Jump Back To Label
 		}
 	}
 	// Read TGA format rgb or rgba image
@@ -189,6 +216,8 @@ public:
 				return FALSE;
 		if(GET_BIT(header.m_ImageDescriptorByte, 5))
 			FlipV();
+		// flip red and blue components
+		FlipC();
 		return TRUE; 
 	}
 	BOOL ReadTGA(const char *pchFileName)
@@ -198,7 +227,7 @@ public:
 	}
 	GLuint GetPixelFormat() const
 	{
-		switch(uComps)
+		switch(m_uComps)
 		{
 		case PIXEL_COMP_RGBA:
 			return GL_RGBA;
@@ -210,12 +239,99 @@ public:
 	}
 	void Draw(float x, float y)
 	{
-		if(!uWidth)
+		if(!m_uWidth)
 			return;
-		SetMemAlign(uWidth, FALSE);
+		SetMemAlign(m_uWidth, FALSE);
 		glRasterPos2f(x, y);
-		glDrawPixels(uWidth, uHeight, GetPixelFormat(), GL_UNSIGNED_BYTE, GetDataPtr());
+		glDrawPixels(m_uWidth, m_uHeight, GetPixelFormat(), GL_UNSIGNED_BYTE, GetDataPtr());
 	}
+};
+
+class Texture
+{
+protected:
+	GLuint				id; // GL Texture Identification
+public:
+	BOOL				mipmapped;
+	GLint				width; // For Not Mipmapped Must be 2^n + 2(border) for some integer n. 
+	GLint				height;	// For Not Mipmapped Must be 2^m + 2(border) for some integer m. 
+	GLint				levelDetail; // The level-of-detail number. Level 0 is the base image level. Level n is the nth mipmap reduction image. 
+    GLint				childs; // The number of color childs in the texture. Must be 1, 2, 3, or 4. 	
+	GLint				border;	// The width of the border. Must be either 0 or 1. 
+    GLuint				format; // GL_COLOR_INDEX, GL_STENCIL_INDEX, GL_DEPTH_COMPONENT,GL_RED, GL_GREEN, GL_BLUE, GL_ALPHA, GL_RGB, GL_RGBA, GL_LUMINANCE,GL_LUMINANCE_ALPHA, GL_BGR_EXT, GL_BGRA_EXT 
+    GLuint				dataFormat; // GL_UNSIGNED_BYTE, GL_BYTE, GL_BITMAP, GL_UNSIGNED_SHORT, GL_SHORT, GL_UNSIGNED_INT, GL_INT, and GL_FLOAT	
+    BYTE				*data; // image data
+	GLint				magFilter; // Mag Filter To Use (GL_NEAREST, GL_LINEAR)
+	GLint				minFilter; // Min Filter To Use (GL_NEAREST, GL_LINEAR)
+	GLint				wrap; // Clamped or Repeated (GL_CLAMP, GL_REPEAT)
+	int					error;
+
+	Texture():
+		width(0), height(0), id(0),
+		minFilter(GL_LINEAR), magFilter(GL_LINEAR), wrap(GL_REPEAT),
+		childs(3), data(NULL), dataFormat(GL_UNSIGNED_BYTE), format(GL_RGB)
+	{}
+	~Texture()
+	{
+		Destroy();
+	}
+	BOOL Create(const Image &image)
+	{
+		if(!image.GetDataSize())
+			return FALSE;
+		GLuint format = image.GetPixelFormat();
+		GLint width = image.GetWidth();
+		GLint height = image.GetHeight();
+		const char *data = image.GetDataPtr();
+
+		glGenTextures(1, &id);
+		glBindTexture(GL_TEXTURE_2D, id);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+
+		if(mipmapped)
+		{
+			gluBuild2DMipmaps(
+				GL_TEXTURE_2D,	// 2D image
+				childs,			// number of color childs
+				width,			// width
+				height,			// height
+				format,			// color model
+				GL_UNSIGNED_BYTE, // data word type
+				data);			// data
+		}
+		else
+		{
+			glTexImage2D(
+				GL_TEXTURE_2D,	// 2D image
+				levelDetail,	// detail level
+				childs,			// number of color childs
+				width,			// width
+				height,			// height
+				border,			// border
+				format,			// color model
+				GL_UNSIGNED_BYTE, // data word type
+				data);			// data
+		}
+		//GL_INVALID_VALUE
+
+		GLenum error = glGetError();
+		return error ? FALSE : TRUE;
+	}
+	void Destroy()
+	{
+		id=0;
+		glDeleteTextures(1, &id);
+	}
+	BOOL Bind() const
+	{
+		glBindTexture(GL_TEXTURE_2D, id);
+		GLenum error = glGetError();
+		return error ? FALSE : TRUE;
+	}
+	int GetID() const { return id; }
 };
 
 void Print(char *fmt, ...)
@@ -244,12 +360,15 @@ HDC hDC = NULL;   // Device Context
 HGLRC hRC = NULL; // Rendering Context
 BOOL bFullscreen = FALSE;
 BOOL bVisible = TRUE;
-BOOL bUpdated = 0;
+BOOL bUpdated = TRUE; // Will force redraw
 LPTSTR pchName = "Arkanoid";
 LONG nLeft = 400, nLastLeft = 0;		// Left Position
 LONG nTop = 300, nLastTop = 0; 		// Top Position
 LONG nWinWidth = 800;
 LONG nWinHeight = 600;
+float fPerspAngle = 45;
+float fPerspNearZ = 0.01f;
+float fPerspFarZ = 100.0f;
 RECT rect; 		      // oustide borders
 DWORD nStyle = 0;
 DWORD nExtStyle = 0;
@@ -259,9 +378,19 @@ BOOL bIsProgramLooping = TRUE;
 BOOL bCreateFullScreen = FALSE;
 LONG nDisplayWidth = 800;
 LONG nDisplayHeight = 600; 
-int nClearColor = 0xffffffff;
 bool bLoaded = false;
-Image imgBall;
+Image imgBall, imgTexture;
+Texture tTexture;
+GLfloat
+	pLightAmbient[]= { 0.5f, 0.5f, 0.5f, 1.0f }, // Ambient Light Values
+	pLightDiffuse[]= { 1.0f, 1.0f, 1.0f, 1.0f }, // Diffuse Light Values
+	pLightPosition[]= { 0.0f, 1.0f, 1.0f, 0.0f }, // Light Position
+	pFogColor[]= {0.0f, 0.0f, 0.0f, 1.0f}; // Fog Color
+float fFogStart = 0.0f;
+float fFogEnd = 1.0f;
+float fFogDensity = 0.08f;
+GLenum uFogMode = GL_EXP;
+GLenum uFogQuality = GL_DONT_CARE;
 
 void Message(char *fmt, ...)
 {
@@ -398,15 +527,17 @@ public:
 	}
 } timer;
 
+
+BOOL ReadImage(Image &image, const char *pchFilename)
+{
+	if( image.ReadTGA(pchFilename) )
+		return TRUE;
+	Print("Error reading image %s!", pchFilename);
+	return FALSE;
+}
+
 void Update()
 {
-	if( !bLoaded )
-	{
-		bLoaded = true;
-		if( !imgBall.ReadTGA("art/ball.tga") )
-			Print("Error reading image!");
-	}
-
 	if( dInput.size() > 0 )
 	{
 		Input input;
@@ -448,10 +579,162 @@ void Update()
 	}
 	bUpdated = TRUE;
 }
+
+void Draw2D()
+{
+	imgBall.Draw(0, 0);
+}
+
+GLvoid DrawSphere(float R, int nDivs)
+{
+	if(R < 0 || nDivs <= 0)
+		return;
+	int nLat = nDivs;
+	int nLon = 2 * nDivs;
+	float
+		u = 0, v = 0, du = 1.0f/nLon, dv = 1.0f/nLat, 
+		N[3], 
+		lat = -0.5*PI, lon = 0, 
+		dlat = PI/nLat, dlon = 2*PI/nLon, 
+		clat, slat, 
+		clat0 = 0, slat0 = -1, 
+		clon, slon, 
+		clon0 = 1, slon0 = 0;
+
+	glBegin(GL_QUADS); // Start Drawing Quads
+	for(int i = 0;i < nLat;i++)
+	{
+		// lat update
+		lat += dlat;
+		clat = cosf(lat);
+		slat = sinf(lat);
+		
+		u = 0;
+		for(int j = 0;j < nLon;j++)
+		{
+			// lon update
+			lon += dlon;
+			clon = cosf(lon);
+			slon = sinf(lon);
+			
+			///////////////////////////////////////////
+			// 
+			//		(u, v+dv)	(u+du, v+dv)
+			//		P3----------P2	lon
+			//		|			|
+			//		|			|	/\
+			//		|			|
+			//		P0----------P1	lon0
+			//		(u, v)		(u+du, v)	
+			//		lat0	>	lat
+			//
+			///////////////////////////////////////////
+
+			// P0: lat = lat0, lon = lon0
+			N[0] = clon0*clat0;
+			N[1] = slon0*clat0;
+			N[2] = slat0;
+			glNormal3d(N[0], N[1], N[2]);
+			glTexCoord2d(u, v);
+			glVertex3d(N[0]*R, N[1]*R, N[2]*R);
+			
+			// P1: lat = lat0, lon = lon
+			N[0] = clon*clat0;
+			N[1] = slon*clat0;
+			N[2] = slat0;
+			glNormal3d(N[0], N[1], N[2]);
+			glTexCoord2d(u+du, v);
+			glVertex3d(N[0]*R, N[1]*R, N[2]*R);
+
+			// P2: lat = lat, lon = lon
+			N[0] = clon*clat;
+			N[1] = slon*clat;
+			N[2] = slat;
+			glNormal3d(N[0], N[1], N[2]);
+			glTexCoord2d(u+du, v+dv);
+			glVertex3d(N[0]*R, N[1]*R, N[2]*R);
+
+			// P3: lat = lat, lon = lon0
+			N[0] = clon0*clat;
+			N[1] = slon0*clat;
+			N[2] = slat;
+			glNormal3d(N[0], N[1], N[2]);
+			glTexCoord2d(u, v+dv);
+			glVertex3d(N[0]*R, N[1]*R, N[2]*R);
+			
+			clon0 = clon;
+			slon0 = slon;
+			u += du;
+		}
+		clat0 = clat;
+		slat0 = slat;
+		v += dv;
+	}
+	glEnd();
+}
+
+void Draw3D()
+{
+	tTexture.Bind();
+	DrawSphere(0.5f, 16);
+}
+
 void Draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	imgBall.Draw(0, 0);
+
+	if( !bLoaded )
+		return;
+
+	// 3D
+	glPushAttrib(GL_ENABLE_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_TEXTURE_2D); // Enable Texture Mapping
+	glDisable(GL_COLOR_MATERIAL);
+	glEnable(GL_FOG); // Enables fog
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity ();
+	gluPerspective (fPerspAngle,
+					(float)nWinWidth / nWinHeight,
+					fPerspNearZ,
+					fPerspFarZ);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0, 0, -5.0f);
+	Draw3D();
+	glPopAttrib();
+
+	// 2D
+	glPushAttrib(GL_ENABLE_BIT);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_LIGHTING);
+	glDisable(GL_FOG);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, nWinWidth, 
+			0, nWinHeight, 
+			1, -1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	Draw2D();
+	glPopAttrib();
+}
+
+void Load()
+{
+	if( bLoaded )
+		return;
+	ReadImage(imgBall, "art/ball.tga");
+	if( ReadImage(imgTexture, "art/texture.tga") )
+	{
+		tTexture.minFilter = GL_LINEAR_MIPMAP_NEAREST;
+		tTexture.magFilter = GL_LINEAR;
+		tTexture.mipmapped = TRUE;
+		if( !tTexture.Create(imgTexture) )
+			Print("Error creating texture!");
+	}
+	bLoaded = true;
 }
 
 void Redraw()
@@ -461,35 +744,58 @@ void Redraw()
 	SwapBuffers(hDC);
 }
 
+void CreateLight(GLenum id, GLfloat *ambient, GLfloat *diffuse, GLfloat *position)
+{
+	glLightfv(id, GL_AMBIENT, ambient);				// Setup The Ambient Light
+	glLightfv(id, GL_DIFFUSE, diffuse);				// Setup The Diffuse Light
+	glLightfv(id, GL_POSITION, position);			// Position The Light
+	glEnable(id);                                   // Enable Light One
+}
+
+void CreateFog(GLfloat start, GLfloat end, GLfloat *color, GLfloat density, GLuint func, GLenum quality)
+{
+	glFogi(GL_FOG_MODE, func);
+	glFogf(GL_FOG_DENSITY, density);
+	glFogf(GL_FOG_START, start);
+	glFogf(GL_FOG_END, end); 
+	glFogfv(GL_FOG_COLOR, color);
+	glHint(GL_FOG_HINT, quality);
+}
+
 BOOL glCreate()
 {
-	// Disable 3D specific features
-	glDisable(GL_DEPTH_TEST);
-	glDisable(GL_LIGHTING);
-
 	// Enable transparent colors
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	// Enables Smooth Shading
+	glShadeModel(GL_SMOOTH);
+	// Specify depth params
+	glClearDepth(1);
+	glDepthFunc(GL_LEQUAL);
+	// Really Nice Perspective Calculations
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	// Create light 1
+	CreateLight(GL_LIGHT1, pLightAmbient, pLightDiffuse, pLightPosition);
+
+	CreateFog(fFogStart, fFogEnd, pFogColor, fFogDensity, uFogMode, uFogQuality);
 
 	// Set the clear color
-	BYTE *rgb = INT_TO_BYTE(nClearColor);
-	glClearColor(	rgb[RED] / 255.0f, 
-					rgb[GREEN] / 255.0f, 
-					rgb[BLUE] / 255.0f, 
-					rgb[ALPHA] / 255.0f);
+	glClearColor(	pFogColor[RED], 
+					pFogColor[GREEN], 
+					pFogColor[BLUE], 
+					pFogColor[ALPHA]);
 
 	return TRUE;
 }
+
 void glDestroy()
 {}
+
 void Reshape()
 {
 	glViewport (0, 0, nWinWidth, nWinHeight);
-	glMatrixMode (GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, nWinWidth, 
-			0, nWinHeight, 
-			1, -1);
+	Redraw();
 }
 
 void InitWindow()
@@ -826,6 +1132,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLin
 			}
 			else
 			{
+				Load();
 				for(;;)
 				{
 					MSG msg; // Message Info
